08a5a062a9b2fa916adc3d8ec415776a8ffe6b1f
root <root@Vin.localdomain>
2022-10-16 16:24:28 +0700

    firstcommit

---
+++ pert4/tugas1.py
@@ -0,0 +1,7 @@
+nama = str(input('masukkan nama anda:        '))
+thnlhr = int(input("input tahun lahir anda:      "))
+thnskrg = 2022
+
+umur = thnskrg - thnlhr
+
+print('jadi usia', nama, 'adalah: ', umur, 'tahun')
---
+++ pert4/tugas2.py
@@ -0,0 +1,8 @@
+nama = str(input('masukkan nama barang:        '))
+hrg = float(input("harga barang:        "))
+dpp = hrg/10
+
+ppn  = hrg + dpp
+
+print('maka harga', nama, 'adalah:      ', ppn, 'rupiah')
+
